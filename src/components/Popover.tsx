437397bc885b822a8915fce80564c5c6031b71a0
Luisa-Lisanne Forck <lforck@thinkport.digital>
2025-05-05 11:23:28 +0200

    Implement custom sticker component with popover

--- src/components/Popover.tsx
+++ src/components/Popover.tsx
@@ -5,6 +5,7 @@ import { useOutsideClick } from "../hooks/useOutsideClick";
 interface PopoverProps {
   sticker: StickerData;
 }
+
 function Popover(props: PopoverProps) {
   const { sticker } = props;
   const [showInfo, setShowInfo] = useState(false);
@@ -14,10 +15,12 @@ function Popover(props: PopoverProps) {
   // handle click on sticker
   const handleClick = () => {
     setShowInfo(!showInfo);
+    console.log("clicked ", triggerRef.current);
   };
 
   // use custom hook to close popover when clicking outside
   const popoverRef = useOutsideClick(() => {
+    console.log("clicked outside", popoverRef.current);
     setShowInfo(false);
   });
 
@@ -34,17 +37,24 @@ function Popover(props: PopoverProps) {
           key={sticker.id}
           src={sticker.src}
           alt={sticker.title}
-          className={`border-box relative rounded object-contain ${sticker.id % 2 ? "rotate-10" : "-rotate-10"}`}
+          className={`border-box relative rounded object-contain ${sticker.id % 2 ? "w-48 rotate-10" : "w-52 -rotate-10"}`}
         />
 
         {/* Popover (appears on click, closes when leaving the element or clicking on another one) */}
         {showInfo && (
           <div
             ref={popoverRef}
-            onMouseLeave={() => setShowInfo(false)}
             className={`absolute z-25 mx-[-70px] flex max-w-96 flex-col gap-2 bg-white p-4 text-xs shadow-lg lg:text-sm ${sticker.id <= 10 ? "my-[-20px]" : "my-[-400px]"}`}
           >
-            <h2 className="font-bold text-pink-500">{sticker.title}</h2>
+            <span className="flex justify-between">
+              <h2 className="font-bold text-pink-500">{sticker.title}</h2>
+              <button
+                className="h-6 border px-2 py-1 text-orange-500 hover:cursor-pointer"
+                onClick={() => setShowInfo(!showInfo)}
+              >
+                {showInfo ? "close" : "open"}
+              </button>
+            </span>
             <p>{sticker.text}</p>
             {sticker.url && (
               <a
--- src/components/StickerListItem.tsx
+++ src/components/StickerListItem.tsx
@@ -3,31 +3,33 @@ import { StickerData } from "../types/StickerData";
 
 function StickerListItem(props: StickerData) {
   const { id, src, title, text, url } = props;
-  const [showText, setShowText] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
 
   return (
     <div className="bg-purple-200 p-1">
       <div key={id} className="flex justify-between">
         <div className="flex items-center gap-4 p-1">
-          {!showText && (
+          {!showInfo && (
             <img
               className={`w-12 object-contain ${id % 2 ? "rotate-10" : "-rotate-15"}`}
               src={src}
               alt={title}
             />
           )}
-          <div className={`text-pink-500 ${showText && "mb-2 font-bold"}`}>
+          <div className={`text-pink-500 ${showInfo && "mb-2 font-bold"}`}>
             {title}
           </div>
         </div>
         <button
           className="h-6 border px-2 py-1 text-orange-500 hover:cursor-pointer"
-          onClick={() => setShowText(!showText)}
+          onClick={() => setShowInfo(!showInfo)}
         >
-          {showText ? "less" : "more"}
+          {showInfo ? "close" : "open"}
         </button>
       </div>
-      {showText && (
+
+      {/* expand to show more information on mobile */}
+      {showInfo && (
         <div className="flex gap-1">
           <img
             className={`w-36 object-contain p-2 ${id % 2 ? "rotate-10" : "-rotate-15"}`}
